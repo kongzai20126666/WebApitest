4dfb4a167895f4ce4e829fd67ac234ee5bea7557
kongzai20126666 <314026365@qq.com>
2022-07-23 23:17:47 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApitest.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string aa()
+        {
+            return "123";
+        }
+
+    }
+}
